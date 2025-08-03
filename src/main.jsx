1a3e3fe5973d6255724702ed2ba55b0e4913e0ac
aslihan-inan <a.uzunogullari@gmail.com>
2025-08-04 02:23:01 +0300

    Fix ThemeLanguageContext import path for Linux build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeLanguageProvider } from "./context/ThemeLanguageContext";
+import { ThemeLanguageProvider } from "./context/ThemeLanguageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
